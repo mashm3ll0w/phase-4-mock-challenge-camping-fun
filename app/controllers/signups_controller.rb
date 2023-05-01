14eff095c74e36b64eb33ee3afe7a9c03e972beb
Charles Swaleh <charlswall@live.com>
2023-05-01 17:44:30 +0300

    Created the routes and serialize the Signup Model

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,21 @@
 class SignupsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :signup_invalid
+  def create
+    signup = Signup.create!(signup_params)
+    activity = Activity.find(signup.activity_id)
+    render json: activity, status: :created
+  end
+
+  private
+
+  def signup_params
+    params.permit(:time, :camper_id, :activity_id)
+  end
+
+  def signup_invalid
+    render json: {
+             errors: ["validation errors"]
+           },
+           status: :unprocessable_entity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :signups
+  resources :signups, only: [:create]
   resources :activities, only: %i[index destroy]
   resources :campers, only: %i[index show create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
