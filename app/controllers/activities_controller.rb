094db693ba6c173f59cc61bcd7f6503d6cf3cdf0
Charles Swaleh <charlswall@live.com>
2023-05-01 17:37:33 +0300

    Created the routes and serialize the Activity Model

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,22 @@
 class ActivitiesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :activity_not_found
+  def index
+    render json: Activity.all, status: :ok
+  end
+
+  def destroy
+    activity = find_activity
+    activity.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_activity
+    Activity.find(params[:id])
+  end
+
+  def activity_not_found
+    render json: { error: "Activity not found" }, status: :not_found
+  end
 end
