39bd5def94e1705b5099eb7dc68259535017b65b
Charles Swaleh <charlswall@live.com>
2023-05-01 17:30:09 +0300

    Created the routes and serialize the Camper Model

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,2 +1,41 @@
 class CampersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :camper_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_camper
+  wrap_parameters format: []
+
+  def index
+    campers = Camper.all
+    render json: campers, status: :ok
+  end
+
+  def show
+    camper = find_camper
+    render json: camper, serializer: CampersAndActivitiesSerializer, status: :ok
+  end
+
+  def create
+    camper = Camper.create!(camper_params)
+    render json: camper, status: :created
+  end
+
+  private
+
+  def camper_params
+    params.permit(:name, :age)
+  end
+
+  def find_camper
+    Camper.find(params[:id])
+  end
+
+  def camper_not_found
+    render json: { error: "Camper not found" }, status: :not_found
+  end
+
+  def invalid_camper
+    render json: {
+             errors: ["validation errors"]
+           },
+           status: :unprocessable_entity
+  end
 end
